82d2b5c52222411a7991db4b16d007ba5e4ddfde
nachopereyra-dev <nachopereyra.dev@gmail.com>
2023-04-03 15:59:21 +0200

    seccion-oportunidades-en-proceso

--- src/components/OportunidadPagina.js
+++ src/components/OportunidadPagina.js
@@ -4,12 +4,33 @@ import Footer from './Footer'
 
 let oportunidad1 = {
     id: 1,
-    query: 'oportunidad-1',
-    nombre: 'Oportunidad 1',
+    query: 'oportunidad-productos-organicos',
+    nombre: 'Orgánicos',
     contenido: 'contenido contenido contenido'
 }
 
-let oportunidades = [oportunidad1]
+let oportunidad2 = {
+    id: 1,
+    query: 'oportunidad-productos-plasticos',
+    nombre: 'Plásticos',
+    contenido: 'contenido contenido contenido'
+}
+
+let oportunidad3 = {
+    id: 1,
+    query: 'oportunidad-productos-textiles',
+    nombre: 'Textiles',
+    contenido: 'contenido contenido contenido'
+}
+
+let oportunidad4 = {
+    id: 1,
+    query: 'oportunidad-productos-madera',
+    nombre: 'Madera',
+    contenido: 'contenido contenido contenido'
+}
+
+let oportunidades = [oportunidad1, oportunidad2, oportunidad3, oportunidad4]
 
 function OportunidadPagina(props) {
 
