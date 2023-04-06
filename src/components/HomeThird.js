41719d8ad558ec15560b8e0b997df4eb4453b6f1
nachopereyra-dev <nachopereyra.dev@gmail.com>
2023-04-06 15:43:27 +0200

    home-third-listo

--- src/components/HomeThird.js
+++ src/components/HomeThird.js
@@ -9,22 +9,38 @@ function HomeThird() {
 
     return (
         <>
-            <div className="home-third" id="home-empresas">
+            <div className="general-home-third">
 
-                <div className="titulo-home-third">
-                    <h1>Empresas circulares en Maldonado</h1>
+                <div className="home-third" id="home-empresas">
+
+                    <div className="titulo-home-third">
+                        <h1>Empresas circulares en Maldonado</h1>
+                    </div>
+
+                    <Link to={'./empresas'} className="circulo" onClick={() => onClick('empresas')}>
+                        <p className="ver-listado">Ver listado</p>
+                    </Link>
+                    
                 </div>
 
-                <Link to={'./empresas'} className="circulo" onClick={() => onClick('empresas')}>
-                    <p className="ver-listado">Ver listado</p>
-                </Link>
+                <div className="home-oportunidades">
 
-                <Link className="flecha-home-third" onClick={() => onClick('home-sobre-nosotros')}>
-                    <i className="fa-solid fa-arrow-down"></i>
-                </Link>
+                    <div className="titulo-home-oportunidades">
+                        <h1>Oportunidades circulares</h1>
+                    </div>
 
-                
+                    <Link to={'./oportunidades'} className="rectangulo" onClick={() => onClick('oportunidades')}>
+                        <p className="ver-listado">Ver oportunidades</p>
+                    </Link>
+
+                    <Link className="flecha-home-third" onClick={() => onClick('home-sobre-nosotros')}>
+                        <i className="fa-solid fa-arrow-down"></i>
+                    </Link>
+
+                </div>
+
             </div>
+
         </>
     )
 }
